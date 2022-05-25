966a260369a0551decebb094e6fbe4f389fa115e
aborschuk <alexander.borschuk@hazelcast.com>
2022-05-25 17:33:33 +0300

    added jet enabled

--- deployment/initial_run/variables.tf
+++ deployment/initial_run/variables.tf
@@ -1,7 +1,7 @@
 # license enterprise key
 variable "license_enterprise_key" {
   type    = string
-  default = "S9DOJKjdfMqYBHZ1Q5NEwnlmTPCAb68Wg0iUku2GyX19911011099001901911001919100001092900111113"
+  default = ""
 }
 
 variable "member_count" {
