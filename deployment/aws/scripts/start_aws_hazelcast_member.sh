bcd3a3acd0c7dbb01b8ca0de7c285323b930a63f
aborschuk <alexander.borschuk@hazelcast.com>
2022-05-25 21:25:57 +0300

    fix sql issue

--- deployment/aws/scripts/start_aws_hazelcast_member.sh
+++ deployment/aws/scripts/start_aws_hazelcast_member.sh
@@ -7,6 +7,7 @@ MEMBERS=$3
 
 
 HZ_JAR_URL=https://hazelcast.jfrog.io/artifactory/release/com/hazelcast/hazelcast-enterprise/${HZ_VERSION}/hazelcast-enterprise-${HZ_VERSION}.jar
+SQL_JAR_URL=https://repo1.maven.org/maven2/com/hazelcast/hazelcast-sql/${HZ_VERSION}/hazelcast-sql-${HZ_VERSION}.jar
 
 mkdir -p ${HOME}/jars
 mkdir -p ${HOME}/logs
@@ -19,13 +20,21 @@ pushd ${HOME}/jars
         echo "Hazelcast JAR could NOT be downloaded!"
         exit 1;
     fi
+
+    if wget -q "$SQL_JAR_URL"; then
+        echo "SQL JAR downloaded succesfully."
+    else
+        echo "SQL JAR could NOT be downloaded!"
+        exit 1;
+    fi
 popd
 
 sed -i -e "s/LICENSE_KEY/${LICENSE_KEY}/g" ${HOME}/hazelcast.yaml
 sed -i -e "s/MEMBERS/${MEMBERS}/g" ${HOME}/hazelcast.yaml
 
 
-nohup java -Dhazelcast.enterprise.license.key=${LICENSE_KEY} -Dhazelcast.config=/home/ubuntu/hazelcast.yaml -jar /home/ubuntu/jars/hazelcast-enterprise-${HZ_VERSION}.jar &>> ${HOME}/logs/hazelcast.logs &
+CLASSPATH="${HOME}/jars/hazelcast-enterprise-${HZ_VERSION}.jar:${HOME}/jars/hazelcast-sql-${HZ_VERSION}.jar:${HOME}/hazelcast.yaml"
+nohup java -cp ${CLASSPATH} -server com.hazelcast.core.server.HazelcastMemberStarter &>> ${HOME}/logs/hazelcast.logs &
 sleep 5
 
 
